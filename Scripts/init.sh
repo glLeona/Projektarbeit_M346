a30bf7eb49bc86a6558c77ef5a6482be585f6e89
smelzd@bbcag.ch <smelzd@bbcag.ch>
2025-12-17 15:49:34 +0100

    Fehlerbehebung 2 project directory

--- Scripts/init.sh
+++ Scripts/init.sh
@@ -17,7 +17,7 @@ OUT_BUCKET="project-outbucket$(whoami)"
 # Lambda / Rolle / Projektpfad
 FUNCTION_NAME="faceRecognitionFunction"
 LAMBDA_ROLE="LabRole"
-PROJECT_DIR="$HOME/Users/FaceRecognitionLambda/src/FaceRecognitionLambda"
+PROJECT_DIR="$HOME/Users/facerecognition/src/facerecognition"
 
 ############################
 # Hilfsfunktionen
