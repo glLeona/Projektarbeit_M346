f060ff5cc27246f1664b0a6ce003ca76e60a1232
smelzd@bbcag.ch <smelzd@bbcag.ch>
2025-12-21 15:35:11 +0100

    Environment-Variable für Lambda Funktion angepasst

--- Scripts/init.sh
+++ Scripts/init.sh
@@ -63,7 +63,7 @@ dotnet lambda deploy-function \
   "$FUNCTION_NAME" \
   --function-role "$LAMBDA_ROLE" \
   --region "$AWS_REGION" \
-  --environment-variables "OUTPUT_BUCKET=$OUT_BUCKET"
+  --environment-variables "$OUT_BUCKET"
 
 ############################
 # 3. Lambda-ARN holen
